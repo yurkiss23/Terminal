23d666a43ba192d2b6ab90fa8efe3568a345a3c3
maks sirotkin <maks.2901.22@gmail.com>
2019-05-31 00:26:05 +0300

    add Paswordhash

--- Terminal/Windows/LoginSignWindow.xaml.cs
+++ Terminal/Windows/LoginSignWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -158,6 +159,15 @@ namespace Terminal.Windows
             return true;
         }
 
+        public static string PasswordHash(string plaintext)
+        {
+            HashAlgorithm mhash = new SHA1CryptoServiceProvider();
+            byte[] bytValue = Encoding.UTF8.GetBytes(plaintext);
+            byte[] bytHash = mhash.ComputeHash(bytValue);
+            mhash.Clear();
+            return Convert.ToBase64String(bytHash);
+        }
+
         private void BtnLog_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("login");
@@ -194,7 +204,7 @@ namespace Terminal.Windows
             {
                 MessageBox.Show(ex.Message);
             }
-            Source = list;
+            Source = list;            
             //this.DialogResult = true;
         }
 
@@ -221,7 +231,7 @@ namespace Terminal.Windows
                     Lname = txtLname.Text,
                     Phone = txtPhone.Text,
                     Email = txtEmail.Text,
-                    Password = txtSignPass.Password
+                    Password = PasswordHash(txtSignPass.Password)
                 });
                 _context.SaveChanges();
                 _userList = new List<UserModel>(
@@ -237,13 +247,13 @@ namespace Terminal.Windows
                     }).ToList());
                 tabLogin.Focus();
                 txtLogin.Text = txtEmail.Text;
-                txtLoginPass.Password = txtSignPass.Password;
+                txtLoginPass.Password = PasswordHash(txtSignPass.Password);
                 BtnLog_Click(sender, e);
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
-            }
+            }            
         }
 
         private void LostFocus_Fname(object sender, RoutedEventArgs e)
